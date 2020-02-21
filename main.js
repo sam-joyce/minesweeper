818c50211cbfe1fea5524f01c11352209e0ab60a
Sam Joyce <samjoycesamjoyce@Sams-MacBook-Pro.local>
2020-02-21 12:06:05 +0000

    broken game

--- main.js
+++ main.js
@@ -4,24 +4,13 @@ const makeBoard = (squares) => {
   // empty: Remove all child nodes of the set of matched elements from the DOM.
   $board.empty();
   for(let i = 0; i < squares; i++) {
-    const $square = $(`<div class="square" id="${i}"></div>`)
+    const $square = $(`<div class="square" id="${i}"></div>`);
     if (Math.random() < 0.1) {
       $square.addClass('bomb');
     }
     $board.append($square);
   }
 
-  // for (let i = 0; i < rows; i++) {
-  //   const $row = $(`<div class = "row" id = "${i}"></div>`);
-  //   for (let j = 0; j < columns; j++) {
-  //     const $column = $(`<div class = "column hidden" value = "${j}"></div>`);
-  //     if (Math.random() < 0.1) {
-  //       $column.addClass('bomb');
-  //     }
-  //     $row.append($column);
-  //   }
-  //   $board.append($row);
-  // }
 }
 
 const resetGame = () => {
@@ -46,38 +35,29 @@ const checkSurroundingCells = ($cell) => {
   let cellsAroundClick = [];
   let counter = 0;
   const square = parseInt($cell.attr("id"));
-  // console.log('help');
   if ($(`#${square - 10}`).hasClass('bomb')) {
     cellsAroundClick.push((square-10));
-    // console.log(cellsAroundClick);
   }
   if ($(`#${square - 9}`).hasClass('bomb')) {
     cellsAroundClick.push((square-9));
-    // console.log(cellsAroundClick);
   }
   if ($(`#${square + 1}`).hasClass('bomb')) {
     cellsAroundClick.push((square+1));
-    // console.log(cellsAroundClick);
   }
   if ($(`#${square + 11}`).hasClass('bomb')) {
     cellsAroundClick.push((square+11));
-    // console.log(cellsAroundClick);
   }
   if ($(`#${square + 10}`).hasClass('bomb')) {
     cellsAroundClick.push((square+10));
-    // console.log(cellsAroundClick);
   }
   if ($(`#${square + 9}`).hasClass('bomb')) {
     cellsAroundClick.push((square+9));
-    // console.log(cellsAroundClick);
   }
   if ($(`#${square - 1}`).hasClass('bomb')) {
     cellsAroundClick.push((square-1));
-    // console.log(cellsAroundClick);
   }
   if ($(`#${square - 11}`).hasClass('bomb')) {
     cellsAroundClick.push((square-11));
-    // console.log(cellsAroundClick);
   }
   console.log(cellsAroundClick);
 
@@ -86,36 +66,20 @@ const checkSurroundingCells = ($cell) => {
     counter++;
   }
 
-  
-  // if ($cell.eq(square).hasClass('bomb')) {
-  //   alert('bomb to the right!');
-  // }
-  // get the cell.value
-  // look at cell[row][value]
-  // look at cell[row][value + 1]
-  // look at cell[row][value - 1]
-
-  // look at cell[row + 1][value]
-  // look at cell[row + 1][value + 1]
-  // look at cell[row + 1][value - 1]
-
-  // look at cell[row - 1][value]
-  // look at cell[row - 1][value + 1]
-  // look at cell[row - 1][value - 1]
   return $cell.text(counter);
 } 
 
 const clicking = $board.on('click', '.square', function() {
   const $cell = $(this);
   if ($cell.hasClass('bomb')) {
-    $(this).append(`<img src="./bomb-img.jpg" />`);
+    $cell.addClass("showBomb");
+    // $cell.append(`<img src="./bomb-img.jpg"/>`);
     return gameOver(false);
   } else {
     const dangerNumber = checkSurroundingCells($cell);
     console.log($(this));
     console.log(dangerNumber);
   }
-  
   
 })
 
