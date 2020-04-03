a61fd2b69daf6b4ff2640689ee792c83cfdaf3c9
Sam Joyce <samjoycesamjoyce@Sams-MBP.home>
2020-04-03 15:05:28 +0100

    recursive function added and game working

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-const $board = $('#board'); 
 
 const makeBoard = (squares) => {
   // empty: Remove all child nodes of the set of matched elements from the DOM.
-  $board.empty();
+  $('#board').empty();
   for(let i = 0; i < squares; i++) {
     const $square = $(`<div class="square" id="${i}"></div>`);
-    if (Math.random() < 0.1) {
+    if (Math.random() < 0.2) {
       $square.addClass('bomb');
     }
-    $board.append($square);
+    $('#board').append($square);
   }
-
+  $('#board').children().each((i, square) => {
+    if (!$(square).hasClass("bomb")) calculateCellScores($(square));
+  })
 }
 
 const resetGame = () => {
@@ -29,10 +30,9 @@ const gameOver = (hasWon) => {
   // resetGame();
 }
 
-const checkSurroundingCells = ($cell) => {
+const calculateCellScores = ($cell) => {
   const square = parseInt($cell.attr("id"));
   let cellsToCheck = [(square-10), (square-9), (square+1), (square+11), (square+10), (square+9), (square-1), (square-11)];
-  // console.log(cellsToCheck);
   let cellsAroundClick = [];
   // const square = parseInt($cell.attr("id"));
   for (let a = 0; a < cellsToCheck.length; a++) {
@@ -42,17 +42,32 @@ const checkSurroundingCells = ($cell) => {
     }
   }
   // console.log(cellsAroundClick);
-
-  return $cell.text(cellsAroundClick.length);
+  $cell.attr("data-score", cellsAroundClick.length)
 } 
 
-const clicking = $board.on('click', '.square', function() {
+const checkCell = ($cell) => {
+  if ($cell.hasClass("checked")) return;
+  const square = parseInt($cell.attr("id"));
+  console.log(square);
+  const score = $cell.attr("data-score");
+  $cell.text(score);
+  $cell.addClass("checked");
+
+  let cellsToCheck = [(square-10), (square-9), (square+1), (square+11), (square+10), (square+9), (square-1), (square-11)];
+  if (score === "0") {
+    cellsToCheck.forEach(id => {
+      checkCell($(`#${id}`))
+    })
+  }
+}
+
+const clicking = $('#board').on('click', '.square', function() {
   const $cell = $(this);
   if ($cell.hasClass('bomb')) {
     $(".bomb").addClass("showBomb");
     gameOver(false);
   } else {
-    const dangerNumber = checkSurroundingCells($cell);
+    checkCell($cell);
   }
   
 })
